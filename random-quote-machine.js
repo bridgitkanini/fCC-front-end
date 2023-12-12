df598122313a9ed44886427d1d136f533a759f3f
bkm-code <144491053+bkm-code@users.noreply.github.com>
2023-12-12 18:12:51 +0300

    Workflow comment

---
+++ random-quote-machine.js
@@ -0,0 +1,23 @@
+/* Random Quote Machine:
+HTML
+Quotes = #quote-box, #text, #author
+Placement of quotes on page = #quote-box
+Icon styles: quote(") icon, twitter icon(and link) = #tweet-quote
+New button create -> New button function; onClick, move to new quote. = #new-quote
+Color; Background + Quote -> Color change, each quote different color.
+
+1. React Component:
+-Class
+-Methods
+-Render
+
+2. Redux:
+-Action
+-Action Creator
+-Reducer
+-Store
+
+3. React-Redux:
+-Provider; wrapper component, rendered.
+-Connect; mapStateToProps(), mapDispatchToProps()
+*/
