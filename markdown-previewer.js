7398b927f2f1ae660fb7aed920fb7d7c96190265
bkm-code <144491053+bkm-code@users.noreply.github.com>
2023-12-14 15:54:00 +0300

    New

---
+++ markdown-previewer.js
@@ -0,0 +1,59 @@
+// !! IMPORTANT README:
+
+// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
+
+/***********
+INSTRUCTIONS:
+  - Select the project you would 
+    like to complete from the dropdown 
+    menu.
+  - Click the "RUN TESTS" button to
+    run the tests against the blank 
+    pen.
+  - Click the "TESTS" button to see 
+    the individual test cases. 
+    (should all be failing at first)
+  - Start coding! As you fulfill each
+    test case, you will see them go   
+    from red to green.
+  - As you start to build out your 
+    project, when tests are failing, 
+    you should get helpful errors 
+    along the way!
+    ************/
+
+// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
+
+// Once you have read the above messages, you can delete all comments. 
+
+/*
+Markdown Previewer; Pseudocode:
+HTML:
+- Textarea; #editor + font awesome fCC logo
+- Div; #previewer + font awesome fCC logo
+- Maximize/minimize button
+
+
+In #textarea, include: 
+i. a heading element (H1 size), 
+ii. a sub heading element (H2 size), 
+iii. a link, 
+iv. inline code, 
+v. a code block, 
+vi. a list item, 
+vii. a blockquote, 
+viii. an image, and 
+ix. bolded text.
+
+Javascript: (Yes, React-Redux)
+- Funtion to link #editor & #previewer; content in #editor is updated in #previewer
+- Import markdown library for content to render as HTML (https://cdnjs.com/libraries/marked)
+- Method to handleChange() maximize and minimize buttons onClick.
+
+CSS:
+Color Gradients;same color different intensities.
+i. #editor #previewer tabs
+ii. content box
+iii. background
+iv. code backgrounds #ffffff
+*/
