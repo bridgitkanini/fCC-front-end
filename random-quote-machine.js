2e42aa3841224f99f462470e181bd72e356b5839
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-01-03 12:31:46 +0300

    Update random-quote-machine.js

--- random-quote-machine.js
+++ random-quote-machine.js
@@ -129,21 +129,13 @@ const QuoteBox = ({ quote, handleNewQuote }) => (
           setQuote(quoteData[getRandomQuote()]);
           setPageBackgroundColor(colors[getRandomColor()]);
       };
-
-      /*
-    const [color, setColor] = React.useState(colors[getRandomColor()]);
-
-    const handleNewColor = () => {
-        setQuote(colors[getRandomColor()])
-    };
-    */
   
       return (
           <div className="main" style={{ 
             backgroundColor: pageBackgroundColor,
             transition: 'background-color 1s ease-in-out, opacity 1s ease-in-out',
           }}>
-              <QuoteBox quote={quote} handleNewQuote={handleNewQuote} /*handleNewColor={handleNewColor}? No. Updated.*/ />
+              <QuoteBox quote={quote} handleNewQuote={handleNewQuote} />
           </div>
       );
   };
@@ -152,62 +144,31 @@ const QuoteBox = ({ quote, handleNewQuote }) => (
 
 
 
-
-
-// !! IMPORTANT README:
-
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
-
-/***********
-INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank 
-    pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go   
-    from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
-    along the way!
-    ************/
-
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
-
-// Once you have read the above messages, you can delete all comments. 
-
-
 /* Pseudocode; Random Quote Machine:
 HTML
 Quotes = #quote-box, #text, #author
 Placement of quotes on page = #quote-box
 Link to tweet quote = #tweet-quote (<a href="twitter.com/intent/tweet" />)
-Icon styles: quote(") icon, twitter icon = #tweet-quote
+Icon styles: quote(") icon, twitter icon = #tweet-quote (font awesome)
 New button create -> New button function; onClick, move to new quote. = #new-quote
 Color; Background + Quote -> Color change, each quote different color.
 
 1. React Component:
--Class; this-state
+-Class; this-state (React.useState, more comprehensive, usually sufficient for non-complex apps)
 -Methods
 -Render
 
 2. Redux:
 -Action
--Action Creator
--Reducer
--Store
+-Action Creator ; quoteBox, colors
+-Reducer ; QuoteBox
+-Store ; 
 
 3. React-Redux:
--Provider; wrapper component, rendered.
--Connect; mapStateToProps(), mapDispatchToProps()
+-Provider; wrapper component, rendered. ; ReactDOM.render
+-Connect; mapStateToProps(), mapDispatchToProps() (??)
 
 CSS:
 -Transitions
--Background color is #text color is buttons color but #quote-box consistently remains #fff 
+-Background color is #text color is buttons color but #quote-box consistently remains #fff (partly achieved.) 
 */
