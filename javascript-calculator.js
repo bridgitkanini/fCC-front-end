9b7e509663fa8d8667bd8e5f67066dcf48726766
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-01-08 21:43:39 +0300

    Update javascript-calculator.js

--- javascript-calculator.js
+++ javascript-calculator.js
@@ -24,3 +24,11 @@ const calcData = [
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const operators = ["AC", "/", "x", "+", "-", "="];
+
+const App = () => {
+    return (
+        <div>JavaScript Calculator</div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector("#app"))
