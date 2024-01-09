61aa8d1105292c8b3fe5d7633134275ae0d94334
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-01-09 19:54:52 +0300

    Update 25+5-clock.js

--- 25+5-clock.js
+++ 25+5-clock.js
@@ -4,7 +4,36 @@ import ReactDOM from "https://esm.sh/react-dom";
 const App = () => {
     return (
         <div>
-            <h1>25 + 5 Clock</h1>
+            <div className="wrapper">
+                <h2>25 + 5 Clock</h2>
+                <div className="break-session-length">
+                    <div>
+                        <h3 id="break-label">Break Length</h3>
+                        <div>
+                            <button id="break-increment">Increase</button>
+                            <strong id="break-length"></strong>
+                            <button id="break-decrement">Decrease</button>
+                        </div>
+                    </div>
+                    <div>
+                        <h3 id="session-label">Session Length</h3>
+                        <div>
+                            <button id="session-increment">Increase</button>
+                            <strong id="session-break"></strong>
+                            <button id="session-decrement">Decrease</button>
+                        </div>
+                    </div>
+               </div>
+               <div className="timer-wrapper">
+                    <div className="timer">
+                        <h2 id="timer-label"></h2>
+                        <h3 id="time-left"></h3>
+                    </div>
+                    <button id="start_stop">Start/Stop</button>
+                    <button id="reset">Reset</button>
+               </div>
+            </div>
+            <audio id="beep" preload="auto" src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav" />
         </div>
     );
 };
