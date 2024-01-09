0368e3a97c57ce522007e6faeb11f01f4028f886
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-01-09 19:15:41 +0300

    Create 25+5-clock.js

---
+++ 25+5-clock.js
@@ -0,0 +1,12 @@
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom";
+
+const App = () => {
+    return (
+        <div>
+            <h1>25 + 5 Clock</h1>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
