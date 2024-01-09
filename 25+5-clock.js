49b96e70d1463e87e902064161ce2ee186b93328
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-01-09 20:22:05 +0300

    Update 25+5-clock.js

--- 25+5-clock.js
+++ 25+5-clock.js
@@ -2,6 +2,24 @@ import React from "https://esm.sh/react";
 import ReactDOM from "https://esm.sh/react-dom";
 
 const App = () => {
+    const handleBreakIncrease = {};
+
+    const breakLength = {};
+
+    const handleBreakDecrease = {};
+
+    const handleSessionIncrease = {};
+        
+    const sessionLength = {};
+
+    const handleSessionDecrease = {};
+
+    const play = {};
+
+    const handlePlay = {};
+
+    const handleReset = {};
+
     return (
         <div>
             <div className="wrapper">
@@ -10,27 +28,27 @@ const App = () => {
                     <div>
                         <h3 id="break-label">Break Length</h3>
                         <div>
-                            <button id="break-increment">Increase</button>
-                            <strong id="break-length"></strong>
-                            <button id="break-decrement">Decrease</button>
+                            <button id="break-increment" disabled={play} onClick={handleBreakIncrease}>Increase</button>
+                            <strong id="break-length">{breakLength}</strong>
+                            <button id="break-decrement" disabled={play} onClick={handleBreakDecrease}>Decrease</button>
                         </div>
                     </div>
                     <div>
                         <h3 id="session-label">Session Length</h3>
                         <div>
-                            <button id="session-increment">Increase</button>
-                            <strong id="session-break"></strong>
-                            <button id="session-decrement">Decrease</button>
+                            <button id="session-increment" disabled={play} onClick={handleSessionIncrease}>Increase</button>
+                            <strong id="session-break">{sessionLength}</strong>
+                            <button id="session-decrement" disabled={play} onClick={handleSessionDecrease}>Decrease</button>
                         </div>
                     </div>
                </div>
                <div className="timer-wrapper">
                     <div className="timer">
-                        <h2 id="timer-label"></h2>
-                        <h3 id="time-left"></h3>
+                        <h2 id="timer-label">{title}</h2>
+                        <h3 id="time-left">{timeFormatter()}</h3>
                     </div>
-                    <button id="start_stop">Start/Stop</button>
-                    <button id="reset">Reset</button>
+                    <button id="start_stop" onClick={handlePlay}>Start/Stop</button>
+                    <button id="reset" onClick={handleReset}>Reset</button>
                </div>
             </div>
             <audio id="beep" preload="auto" src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav" />
